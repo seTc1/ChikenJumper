d5818902ea40aec470009e474b9242d78812c1e8
seTc1 <sema2234.archi@gmail.com>
2025-01-22 19:05:13 +0300

    [1.20] Chiken Idle

--- PitchJumperProject/constants.py
+++ PitchJumperProject/constants.py
@@ -4,18 +4,29 @@ SCREEN_HEIGHT = 1080  # Высота окна
 FULLSCREEN = True  # Полноэкранный режим
 FPS = 60  # Количество кадров в секунду
 
-# Настройки тайлов
+# Настройки уровня и тайлов
 TILE_SIZE = 64  # Размер одной клетки тайла
 LEVEL_NAMES = ["Level_1.txt", "Level_2.txt"]  # Файл с уровнем
 TEXTURE_FOLDER = "Textures"  # Папка с текстурами
 LEFT_ARROW_SPRITE = "Textures\LeftArrow.png"  # Папка с текстурами
-FONT_PATH = "pixel.otf"  # Путь к пользовательскому шрифту
+
 FONT_SIZE = 24  # Размер пользовательского шрифта
+
 BACKGROUND_COLOR = (91, 145, 76)  # Цвет фона экрана
-PLAYER_TEXTURE = "ChikenIdle.png"  # Текстура персонажа
-CHIKEN_ANIM_TEXTURES = ["ChikenIdle.png", "ChikenRun_1.png", "ChikenRun_2.png"]
-CHIKEN_ANIM_SPEED = 1
 PASSABLE_TILES = ["PathTile1", "PathTile2", "PathTile3"]  # Проходимые тайлы
-HEART_TEXTURE = "Textures\Heart.png"  # Текстура сердца
+
+# Настройки интерфейса
 HEART_SIZE = 100  # Размер спрайта сердца
 HP_TEXT_SIZE = 50  # Размер текста здоровья
+HEART_TEXTURE = "Textures\Heart.png"  # Текстура сердца
+FONT_PATH = "pixel.otf"  # Путь к шрифту
+
+# Настройки игрока
+PLAYER_TEXTURE = "ChikenIdle.png"  # Текстура персонажа
+
+# Настройки анимаций
+CHIKEN_RUN_ANIM_TEXTURES = ["ChikenIdle.png", "ChikenRun_1.png", "ChikenIdle.png", "ChikenRun_2.png"]
+CHIKEN_RUN_ANIM_SPEED = 4.2
+CHIKEN_IDLE_ANIM_TEXTURES = ["ChikenIdle.png", "ChikenIdle_1.png", "ChikenIdle_2.png", "ChikenIdle_1.png",
+                             "ChikenIdle.png"]
+CHIKEN_IDLE_ANIM_SPEED = 7
--- PitchJumperProject/player_controller.py
+++ PitchJumperProject/player_controller.py
@@ -1,31 +1,37 @@
 import pygame
 import os
-from constants import TILE_SIZE, PLAYER_TEXTURE, PASSABLE_TILES, FPS
+from constants import TILE_SIZE, CHIKEN_RUN_ANIM_TEXTURES, CHIKEN_RUN_ANIM_SPEED, PLAYER_TEXTURE, PASSABLE_TILES, FPS, \
+    TEXTURE_FOLDER, CHIKEN_IDLE_ANIM_SPEED, CHIKEN_IDLE_ANIM_TEXTURES
 
 
 class Player:
     def __init__(self, texture, position, tile_size):
-        self.image = pygame.image.load(texture).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (tile_size, tile_size))
+        self.anim_frames = [pygame.transform.scale(pygame.image.load(os.path.join(TEXTURE_FOLDER, img)).convert_alpha(),
+                                                   (tile_size, tile_size)) for img in CHIKEN_IDLE_ANIM_TEXTURES]
+        self.image = self.anim_frames[0]
         self.x, self.y = position
         self.target_x, self.target_y = self.x, self.y
         self.tile_size = tile_size
         self.game_over = False
         self.hp = 10
         self.moving = False
-        self.speed = TILE_SIZE / (FPS // 5)
+        self.speed = TILE_SIZE / (FPS // 3)
         self.offset_x = 0
         self.offset_y = 0
         self.move_dx = 0
         self.move_dy = 0
+        self.anim_index = 0
+        self.anim_timer = 0
 
     def draw(self, screen, offset_x, offset_y):
-        if self.game_over: return
+        if self.game_over:
+            return
         screen.blit(self.image, ((self.x * self.tile_size + offset_x) + self.offset_x,
                                  (self.y * self.tile_size + offset_y) + self.offset_y))
 
     def move(self, dx, dy, tile_map, screen):
-        if self.game_over or self.moving: return
+        if self.game_over or self.moving:
+            return
         new_x = self.x + dx
         new_y = self.y + dy
 
@@ -37,9 +43,12 @@ class Player:
                 self.moving = True
                 self.move_dx = dx * self.speed
                 self.move_dy = dy * self.speed
+                self.anim_index = 1
+                self.anim_timer = 0
+
                 if tile_map.check_if_end((new_x, new_y)):
                     pass
-                    # Появления финального меню
+
                 total_hp_change = -1
                 if tile_value is not None:
                     total_hp_change += tile_value
@@ -50,6 +59,12 @@ class Player:
         if self.moving:
             self.offset_x += self.move_dx
             self.offset_y += self.move_dy
+            self.anim_timer += 1
+
+            if self.anim_timer >= CHIKEN_RUN_ANIM_SPEED:
+                self.anim_index = (self.anim_index + 1) % len(CHIKEN_RUN_ANIM_TEXTURES)
+                self.image = pygame.transform.scale(pygame.image.load(os.path.join(TEXTURE_FOLDER, CHIKEN_RUN_ANIM_TEXTURES[self.anim_index])).convert_alpha(), (self.tile_size, self.tile_size))
+                self.anim_timer = 0
 
             if abs(self.offset_x) >= TILE_SIZE or abs(self.offset_y) >= TILE_SIZE:
                 self.x = self.target_x
@@ -57,6 +72,14 @@ class Player:
                 self.offset_x = 0
                 self.offset_y = 0
                 self.moving = False
+                self.image = pygame.transform.scale(pygame.image.load(os.path.join(TEXTURE_FOLDER, CHIKEN_IDLE_ANIM_TEXTURES[0])).convert_alpha(), (self.tile_size, self.tile_size))
+
+        else:  # Когда игрок не двигается, воспроизводим анимацию стояния
+            self.anim_timer += 1
+            if self.anim_timer >= CHIKEN_IDLE_ANIM_SPEED:
+                self.anim_index = (self.anim_index + 1) % len(CHIKEN_IDLE_ANIM_TEXTURES)
+                self.image = pygame.transform.scale(pygame.image.load(os.path.join(TEXTURE_FOLDER, CHIKEN_IDLE_ANIM_TEXTURES[self.anim_index])).convert_alpha(), (self.tile_size, self.tile_size))
+                self.anim_timer = 0
 
     def change_hp(self, amount):
         self.hp = max(0, self.hp + amount)
